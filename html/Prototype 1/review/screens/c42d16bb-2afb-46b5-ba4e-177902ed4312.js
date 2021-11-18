969f3501458eaf34b97c43a05e2816cfec25ccc0
Chris <2003880@aluno.univesp.br>
2021-11-18 02:46:44 -0300

    prototipo em html

---
+++ html/Prototype 1/review/screens/c42d16bb-2afb-46b5-ba4e-177902ed4312.js
@@ -0,0 +1,93 @@
+var content='<div class="ui-page" deviceName="web" deviceType="desktop" deviceWidth="1920" deviceHeight="1080">\
+    <div id="t-f39803f7-df02-4169-93eb-7547fb8c961a" class="template growth-both devWeb canvas firer commentable non-processed" alignment="left" name="Template 1" width="1920" height="1080">\
+    <div id="backgroundBox"><div class="colorLayer"></div><div class="imageLayer"></div></div>\
+    <div id="alignmentBox">\
+      <link type="text/css" rel="stylesheet" href="./resources/templates/f39803f7-df02-4169-93eb-7547fb8c961a-1637213083405.css" />\
+      <!--[if IE]><link type="text/css" rel="stylesheet" href="./resources/templates/f39803f7-df02-4169-93eb-7547fb8c961a-1637213083405-ie.css" /><![endif]-->\
+      <!--[if lte IE 8]><![endif]-->\
+      <div class="freeLayout">\
+      </div>\
+\
+      </div>\
+      <div id="loadMark"></div>\
+    </div>\
+\
+    <div id="s-c42d16bb-2afb-46b5-ba4e-177902ed4312" class="screen growth-vertical devWeb canvas PORTRAIT firer ie-background commentable non-processed" alignment="left" name="Contato" width="1920" height="1080">\
+    <div id="backgroundBox"><div class="colorLayer"></div><div class="imageLayer"></div></div>\
+    <div id="alignmentBox">\
+      <link type="text/css" rel="stylesheet" href="./resources/screens/c42d16bb-2afb-46b5-ba4e-177902ed4312-1637213083405.css" />\
+      <!--[if IE]><link type="text/css" rel="stylesheet" href="./resources/screens/c42d16bb-2afb-46b5-ba4e-177902ed4312-1637213083405-ie.css" /><![endif]-->\
+      <!--[if lte IE 8]><link type="text/css" rel="stylesheet" href="./resources/screens/c42d16bb-2afb-46b5-ba4e-177902ed4312-1637213083405-ie8.css" /><![endif]-->\
+      <div class="freeLayout">\
+      <div id="s-Contact-4" class="group firer ie-background commentable non-processed" customid="Contact-4" datasizewidth="1024.0px" datasizeheight="316.0px" >\
+        <div id="s-Bg" class="pie rectangle manualfit firer commentable non-processed" customid="Bg"   datasizewidth="963.0px" datasizeheight="268.0px" datasizewidthpx="963.0000000000013" datasizeheightpx="268.0" dataX="479.0" dataY="763.0" >\
+          <div class="backgroundLayer">\
+            <div class="colorLayer"></div>\
+            <div class="imageLayer"></div>\
+          </div>\
+          <div class="borderLayer">\
+            <div class="paddingLayer">\
+              <div class="content">\
+                <div class="valign">\
+                  <span id="rtr-s-Bg_0"></span>\
+                </div>\
+              </div>\
+            </div>\
+          </div>\
+        </div>\
+        <div id="s-Title" class="pie richtext manualfit firer ie-background commentable non-processed" customid="Fale com a gente"   datasizewidth="306.0px" datasizeheight="100.0px" dataX="807.5" dataY="763.0" >\
+          <div class="backgroundLayer">\
+            <div class="colorLayer"></div>\
+            <div class="imageLayer"></div>\
+          </div>\
+          <div class="borderLayer">\
+            <div class="paddingLayer">\
+              <div class="content">\
+                <div class="valign">\
+                  <span id="rtr-s-Title_0">Fale com a gente</span>\
+                </div>\
+              </div>\
+            </div>\
+          </div>\
+        </div>\
+        <div id="s-Paragraph" class="pie richtext manualfit firer ie-background commentable non-processed" customid="donate.doacoes@gmail.com("   datasizewidth="213.0px" datasizeheight="50.0px" dataX="854.0" dataY="848.0" >\
+          <div class="backgroundLayer">\
+            <div class="colorLayer"></div>\
+            <div class="imageLayer"></div>\
+          </div>\
+          <div class="borderLayer">\
+            <div class="paddingLayer">\
+              <div class="content">\
+                <div class="valign">\
+                  <span id="rtr-s-Paragraph_0">donate.doacoes@gmail.com<br />(13) 99206-2732</span>\
+                </div>\
+              </div>\
+            </div>\
+          </div>\
+        </div>\
+        <div id="s-Paragraph_2" class="pie richtext manualfit firer ie-background commentable non-processed" customid="Rua Josefina S&oacute;rio, 369 -"   datasizewidth="279.0px" datasizeheight="84.0px" dataX="821.0" dataY="924.0" >\
+          <div class="backgroundLayer">\
+            <div class="colorLayer"></div>\
+            <div class="imageLayer"></div>\
+          </div>\
+          <div class="borderLayer">\
+            <div class="paddingLayer">\
+              <div class="content">\
+                <div class="valign">\
+                  <span id="rtr-s-Paragraph_2_0">Rua Josefina S&oacute;rio, 369 - Vila Santa Rosa<br />Guaruj&aacute; - SP<br />11431-170, BRASIL</span>\
+                </div>\
+              </div>\
+            </div>\
+          </div>\
+        </div>\
+      </div>\
+\
+      </div>\
+\
+      </div>\
+      <div id="loadMark"></div>\
+    </div>\
+  \
+</div>\
+';
+document.getElementById("chromeTransfer").innerHTML = content;
