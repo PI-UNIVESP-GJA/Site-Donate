6b02876ca5a26c3bd0d971203f3c884d2b606560
Chris <2003880@aluno.univesp.br>
2021-11-23 02:07:55 -0300

    Teste-flask

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\davi_\Documents\GitHub\Site-Donate\venv
+set VIRTUAL_ENV=C:\Users\chris\OneDrive\Documentos\MeusProjetos\Site-Donate\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
