c786d94416518e5d603d6c8ec22db940b0d960bd
silvachris <2003880@aluno.univesp.br>
2021-11-29 23:30:31 -0300

    pipenv

---
+++ Projeto/app/__init__.py
@@ -0,0 +1,8 @@
+from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+login_manager = LoginManager(app)
+db = SQLAlchemy(app)
--- Projeto/site-donate.py
+++ Projeto/site-donate.py
@@ -1,5 +1,15 @@
 from flask import Flask, render_template
+from app import app
 app = Flask(__name__)
+
+#@app.route('/register', methods=['GET', 'post'])
+#def register():
+#    return render_template('register.html')
+
+#@app.route('/login', methods=['GET', 'post'])
+#def login():
+#    return render_template('login.html')
+
 @app.route("/")
 def homepage():
     return render_template("homepage.html")
